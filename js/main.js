1b4f3323455184b00c4d2a853d4b487ed7a8ecca
HCXAVIERMORAES <hcxaviermoraes@outlook.com>
2021-08-08 17:28:23 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var nome = "Helton c. x. Moraes";
+var idade = 43;
+var idade2 = 30;
+var frase = "Japão é o melhor time do mundo";
+//concatenação usa-se o '+' porem se não houver uma estring ele soma se for inteiro
+//alert("Meu primeiro js");
+//alert(nome + " tem "+ idade +'anos');
+//alert(idade + idade2);
+console.log(nome);
+console.log(frase.replace("Japão", "Brasil"));//subistitui japão por Brasil
+alert(frase.replace("Japão", "Brasil"));//idem
+
